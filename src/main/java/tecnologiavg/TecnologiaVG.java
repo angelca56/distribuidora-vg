a0508e557b6ffafe6428ebf0cb14bf46d87bd544
angelca56 <cabreragironl@gmail.com>
2025-05-25 20:02:31 -0600

    Estructura del proyecto

---
+++ src/main/java/tecnologiavg/TecnologiaVG.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package tecnologiavg;
+
+/**
+ *
+ * @author delma
+ */
+public class TecnologiaVG {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
